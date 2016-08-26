296c5945b480174dd59a5ccd38b24fc0ecf2c251
Midi <midi13@163.com>
2016-08-26 05:38:35 +0000

    authenticate docs

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
     end
 
   end
+
+  get '/docs/index', to: 'docs#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
